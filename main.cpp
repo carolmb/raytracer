a02fa66858fb83a2b071b690f883b654a5db020d
Ana <a_caroline96@hotmail.com>
2017-08-25 19:21:07 -0300

    antialiasing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "inc/img.h"
 #include "inc/render.h"
+#include "inc/vec3.h"
 
 int main() {
 	Scene scene;
-	Color* pxls = Render::defaultRender(600, 1200, scene);
+	Color* pxls = Render::defaultRender(600, 1200, scene, 4);
 	Img img(600, 1200, "test.ppm", "ppm", pxls);
 	img.saveBin(); 
 	std::cout << "Completed" << std::endl;
