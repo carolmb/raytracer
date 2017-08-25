a3217d5774cc55ca14d539898697496ed4cad936
Ana <a_caroline96@hotmail.com>
2017-08-24 22:32:00 -0300

    sphere color using normal

--- inc/sphere.h
+++ inc/sphere.h
@@ -1,20 +1,31 @@
 #ifndef __SPHERE__
 #define __SPHERE__
 
+#include <cmath>
 #include "ray.h"
 
 class Sphere {
 public:
 	Point3 center;
 	double radius;
-	Sphere() : center(Point3(0, 1, -1)), radius(0.5) {}
+	Sphere() : center(Point3(0, 0, -1)), radius(0.5) {}
 
-	bool hit(Ray r) {
+	double hit(Ray r) {
 		Vec3 oc = r.origin() - center;
 		double a = r.dir().len2();
 		double b = 2.0 * oc.dot(r.dir());
 		double c = oc.len2() - (radius*radius);
-		return b*b > 4*a*c;
+		double delta = b*b - 4*a*c;
+		if(delta > 0) {
+			return (-b - std::sqrt(delta)) / (2*a);
+		} else {
+			return -1;
+		}
+	}
+
+	Color getColor(Point3 p) {
+		Vec3 normal = (p - center).norm();
+		return (normal + 1)*0.5;
 	}
 };
 
