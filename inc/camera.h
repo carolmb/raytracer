63c382e7b741e0d44cd95719db2f90dd611ea4c8
Ana Caroline <carolmedeirosbrito@gmail.com>
2017-08-26 19:48:24 -0300

    reading objs from file

--- inc/camera.h
+++ inc/camera.h
@@ -14,6 +14,8 @@ public:
 	Camera() : origin(), 
 		lowerLeftCorner(Vec3(-2, -1, -1)), horizontal(Vec3(4, 0, 0)), vertical(Vec3(0, 2, 0)) {}
 
+	Camera(Vec3 llc, Vec3 h, Vec3 v) : lowerLeftCorner(llc), horizontal(h), vertical(v) {}
+
 	Ray getRay(double u, double v) {
 		return Ray(origin, lowerLeftCorner + horizontal*u + vertical*v - origin);
 	}
