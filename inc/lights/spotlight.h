76c08c8c24a80e0b93e26b44743b5b3ca2a71994
Ana Caroline <carolmedeirosbrito@gmail.com>
2017-10-06 00:31:20 -0300

    spotlight in progress

--- inc/lights/spotlight.h
+++ inc/lights/spotlight.h
@@ -2,19 +2,25 @@
 #define SPOTLIGHT__
 
 #include "light.h"
+#include "ray.h"
 
 #include <cmath>
+#include <iostream>
+#define PI 3.14159265
 
 class SpotLight : public Light {
 	Point3 o;
 	double angle;
 	Vec3 dir;
 public:
-	SpotLight(Vec3 i, Vec3 o, Vec3 d, double a) : Light(i), o(o), dir(d), angle(std::cos(a)) {}
+	SpotLight(Vec3 i, Vec3 o, Vec3 d, double a) : Light(i), o(o), dir(d), angle(std::cos(a*PI / 180.0)) {
+		//std::cout << angle << " ";
+	}
 	Vec3 getDir(Point3 p = Vec3()) { 
-		Vec3 lightDir = -(o - p).norm(); 
-		if(lightDir.dot((dir + o).norm()) > angle) {
-			return lightDir;
+		Vec3 lightRay = (p - o).norm(); 
+		double currentCos = lightRay.dot(dir.norm());
+		if(currentCos > angle) {
+			return lightRay;
 		} 
 		return Vec3();
 	}
