0bb2ae0ce5c43413b00aba7c4ba29ad7c80ac1cc
Ana Caroline <carolmedeirosbrito@gmail.com>
2017-11-08 21:47:27 -0300

    rotation in progress

--- src/objparser.cpp
+++ src/objparser.cpp
@@ -1,10 +1,14 @@
 #include "parsers/objparser.h"
 
+#define PI 3.14159265	
+
 #include "parsers/matparser.h"
 #include "sphere.h"
 #include "triangle.h"
 #include "transf.h"
 #include "matrix.h"
+
+#include <cmath>
 
 bool ObjectParser::readSphere(std::istringstream &reader, std::shared_ptr<Object> &o) {
 	if(!checkFieldName(reader, "CENTER")) return false;
@@ -43,13 +47,35 @@ bool ObjectParser::readTriangle(std::istringstream &reader, std::shared_ptr<Obje
 bool ObjectParser::readTransf(std::istringstream &reader, Mat4 &m, std::string transfName) {
 	if(transfName.compare("scale") == 0) {
 		Vec3 t; readVec3(reader, t);
-		
-	} else if(transfName.compare("translation") == 0) {
+		m.m[0][0] = t.x; m.m[1][1] = t.y; m.m[2][2] = t.z; 
+	} else if(transfName.compare("translate") == 0) {
 		Vec3 t; readVec3(reader, t);
-		m.m[0][3] = t.x; m.m[1][3] = t.y; m.m[2][3] = t.z; m.m[3][3] = 1;
-	} else if(transfName.compare("rotation") == 0) {
-		Vec3 t; readVec3(reader, t);
+		m.m[0][3] = t.x; m.m[1][3] = t.y; m.m[2][3] = t.z;
+	} else if(transfName.compare("rotate") == 0) {
+		std::string axis; 
+		double angle;
+		reader >> axis;
+		reader >> angle;
+		double cos = std::cos(angle*PI/180.0);
+		double sin = std::sin(angle*PI/180.0);
 		
+		if(axis.compare("x") == 0) {
+			m.m[1][1] = cos;
+			m.m[2][2] = cos;
+			m.m[1][2] = -sin;
+			m.m[2][1] = sin;
+			std::cout << m;
+		} else if(axis.compare("y") == 0) {
+			m.m[0][0] = cos;
+			m.m[0][2] = sin;
+			m.m[2][0] = -sin;
+			m.m[2][2] = cos;
+		} else if(axis.compare("z") == 0) {
+			m.m[0][0] = cos;
+			m.m[0][1] = -sin;
+			m.m[1][0] = sin;
+			m.m[1][1] = cos;
+		} else { return false; }
 	} else if(transfName.compare("free") == 0) {
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 4; j++) {
@@ -58,9 +84,7 @@ bool ObjectParser::readTransf(std::istringstream &reader, Mat4 &m, std::string t
 		}
 	} else if(transfName.compare("null") == 0) {
 		// nothing to do
-	} else {
-		return false;
-	}
+	} else { return false; }
 	return true;
 }
 
@@ -83,15 +107,13 @@ bool ObjectParser::readTransformations(std::istringstream &reader, std::shared_p
 	allTransf.identity();
 	if(transf.size() > 0) {
 		allTransf = transf.back();
-		for(int i = transf.size() - 2; i > 0; i--) {
+		for(int i = transf.size() - 2; i >= 0; i--) {
 			allTransf = allTransf*transf[i];
 		}
 	}
 
-	// multiplica ao contrário (pilha)
 	Transformation finalT;
 	finalT.mat = allTransf;
-	std::cout << finalT.mat;
 	finalT.inv = allTransf.inverse();
 	o->transf = finalT;
 	return true;
